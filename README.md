57e42f5018a72472a99864d8ac3c655e9e52eb25
vbentley <vbentley@gmail.com>
2020-03-30 13:10:30 -0700

    Reseeding Project Files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # docker-base-images
+Repo with docker base images that can be used as a starting point for work.
+
+## Python-alpine-pyodbc
+A docker image built from a Python 3.7 Alpine base along with pyodbc and its dependencies to connect to an SQL Server Instance. 
+
+### Notes
+* The image has a footprint of about 275MB. We should be able to optimize it further for a smaller footprint
+* If you're not familiar with Docker, here's what you'll need to do to use the test file
+  * Make sure your connection string is accurate
+  * Copy the file into a container based off the image
+  * From within the container, run the file 
+  * Alternately you could use a docker-compose file to automate the steps above
+ 
+ 
---
+++ python-alpine-pyodbc/test/pyodbc_test.py
@@ -0,0 +1,31 @@
+# TEST SCRIPT TO SEE IF PYODBC INSTALLED ON A PYTHON ALPINE BASE CONNECTS TO A LOCAL INSTANCE OF SQL SERVER.
+
+import pyodbc
+
+# SETUP CONNECTION TO THE SQL SERVER DATABASE
+def pyodbc_localhost():
+    driver = '/usr/lib/libtdsodbc.so'  # NOT THE MOST ELEGANT WAY, BUT WE'RE TELLING PYODBC WHERE TO FIND THE FREETDS DRIVER
+    try:
+        cnxn = pyodbc.connect('DRIVER='+driver+';SERVER=<serverIP or docker.host.internal>;PORT=1433;DATABASE=<dbname>;UID=<dbuser>;PWD=<dbuserpassword>')
+    except Exception as e:
+        print(f"Couldn't connect to db on localhost because {e}")
+    else:
+        cursor = cnxn.cursor()
+        return cnxn, cursor
+
+
+# EXECUTE A SQL QUERY TO SEE IF WE GET A RESULT USING THE CONNECTIONS / CURSOR
+def test_localhost(cursor):
+    try:
+        cursor.execute(""" SELECT * FROM db.schema.table """)  # WHATEVER QUERY YOU WANT TO EXECUTE GOES IN HERE
+    except Exception as e:
+        print(f"Couldn't get data from table because {e}")
+    else:
+        for row in cursor.fetchall():
+            print(str(row))  # REALLY BASIC TEST JUST TO GET AN OUTPUT AND SEE IF WE CAN RETRIEVE A RESULTSET
+
+
+cnxn, cursor = pyodbc_localhost()
+test_localhost(cursor)
+cursor.close()
+cnxn.close()
